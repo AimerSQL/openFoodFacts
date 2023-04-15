d77de69dbd0b3010da819aee3f73a80b8aa81a1d
dSHEN <dshen123456@gmail.com>
2023-04-15 12:50:33 +0200

    swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
 const cors = require('cors');
 
@@ -6,11 +9,28 @@ app.set('port', 4000);
 app.use(cors());
 app.use(express.json());
 
+const options = {
+    swaggerDefinition: {
+        openapi : "3.0.0",
+        info: {
+            title: 'Open Food Facts', // Título de la API
+            version: '1.0.0', // Versión de la API
+            description: 'Documentación de la API de Open Food Facts', // Descripción de la API
+        },
+    },
+    apis: ['./getProducts.js'], // Archivos que contienen la documentación de la API
+};
 
+const swaggerSpec = swaggerJsdoc(options);
 
 //routers
-app.use('/food', require('./routers/food')); //obtener codigo de barra
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
 
+app.use('/food', require('./routers/food')); //obtener codigo de barra
 
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,40 @@
-const app = require('./app');
 require('./conexion');
+const userRoute = require("./getProducts");
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-async function main() {
-    await app.listen(4000);
-    console.log("Server is running on port " + 4000);
-}
+//swagger
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const options = {
+    swaggerDefinition: {
+        openapi : "3.0.0",
+        info: {
+            title: 'Open Food Facts', // Título de la API
+            version: '1.0.0', // Versión de la API
+            description: 'Documentación de la API de Open Food Facts', // Descripción de la API
+        },
+    },
+    apis: ['./getProducts.js'], // Archivos que contienen la documentación de la API
+};
 
-main();
+const swaggerSpec = swaggerJsdoc(options);
+
+app.set('port', 4000);
+app.use(cors());
+app.use(express.json());
+app.use("/api", userRoute);
+
+
+
+//routers
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+app.use('/food', require('./routers/food')); //obtener codigo de barra
+
+app.listen(4000, () => console.log("server running on port ", app.get('port')))
