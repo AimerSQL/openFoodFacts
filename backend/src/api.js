796d9925cf6b21d83eaa04fad2dc3122fc2db509
JunjieLi <dshen123456@gmail.com>
2023-04-19 20:54:11 +0200

    api

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const axios = require('axios');
-const votModel = require('./models/votModel'); // 注意文件路径可能需要根据实际情况进行调整
-
 const router = express.Router();
 const foodController = require('./controllers/foodController'); // 引入 foodController 模块
 
-// 定义 GET 产品 API
 /**
  * @swagger
  * /products:
@@ -15,9 +11,43 @@ const foodController = require('./controllers/foodController'); // 引入 foodCo
  *     responses:
  *       200:
  *         description: 成功获取产品列表
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/products'
+ * 
+ * components:
+ *   schemas:
+ *     products:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The product ID
+ *         image_url:
+ *           type: string
+ *           description: The product image URL
+ *         product_name:
+ *           type: string
+ *           description: The product name
+ *         ingretients_text:
+ *           type: string
+ *         brand:
+ *           type: string
+ *           description: The brand of the product
+ *         categories:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: The categories of the product
+ *         countries_en:
+ *           type: string
+ *           description: The country of origin of the product
  */
+
 router.get('/products', async (req, res) => {
-    // 在这里调用 foodController 模块中的处理函数
     await foodController.getAllFoods(req, res);
 });
 
